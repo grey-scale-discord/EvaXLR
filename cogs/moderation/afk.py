a5d90f015d093430a5eda6cabb5ce2dbf86bdd51
Levi-Ackerman19 <96330121+Levi-Ackerman19@users.noreply.github.com>
2022-03-30 13:42:33 +0530

    Update afk.py

--- cogs/moderation/afk.py
+++ cogs/moderation/afk.py
@@ -20,17 +20,16 @@ class Afk(commands.Cog):
 		try:
 			users = get_afk_data()
 
-			_id = f"<@!{ctx.author.id}>"
-			users[str(_id)] = {}
-			users[str(_id)]["user_name"] = ctx.author.name
-			users[str(_id)]["reason"] = reason if reason!= None else None
+			users[str(ctx.author.id)] = {}
+			users[str(ctx.author.id)]["user_name"] = ctx.author.name
+			users[str(ctx.author.id)]["reason"] = reason if reason!= None else None
 
 			with open("cogs/moderation/afklist.json" , "w") as f:
 				json.dump(users , f , indent = 2)
 
 			await ctx.send(f" {ctx.author.mention} ``is now Afk``- {reason}")
 			self.initial = time.time()
-			self._id = _id
+			self.author = ctx.author
 			self.reason = reason
 
 		except discord.errors.Forbidden:
@@ -40,8 +39,14 @@ class Afk(commands.Cog):
 	async def on_message(self , message):
 		users = get_afk_data()
 
-		re_id = f"<@!{message.author.id}>"
-		if str(re_id) in users.keys() and "afk" not in message.content:
+		mention = [key for key in users.keys()]
+
+		for _mention in mention:
+			print(_mention, message.content)
+			if str(_mention) in message.content and "afk" not in message.content.lower():
+				await message.channel.send(f'<@{_mention}>`` is Afk`` - {users[str(_mention)]["reason"]} ')
+
+		if str(message.author.id) in users.keys() and "afk" not in message.content:
 
 			final = time.time()
 			time_afk_seconds = (final - self.initial)
@@ -53,18 +58,13 @@ class Afk(commands.Cog):
 			else:
 				time_afk = f"{round(time_afk_seconds)} Seconds"
 
-			del users[str(self._id)]
+			del users[str(self.author.id)]
 
 			with open("cogs/moderation/afklist.json", "w") as f:
 				json.dump(users, f, indent=2)
 
 			await message.channel.send(f"Welcome! {message.author.mention} You were Afk for {time_afk}")
 
-		mention = [f"<@!{key}>" for key in users.keys()]
-
-		for _mention in mention:
-			if _mention in message.content:
-				await message.channel.send(f'{users[str(_mention)]["user_id"]}`` is Afk`` - {users[str(key)]["reason"]} ')
 
 def setup(bot):
 	bot.add_cog(Afk(bot))
